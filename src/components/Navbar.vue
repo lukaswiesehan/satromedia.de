3b223a118b14508f5804fa550ce684f54e8dd8fa
lukaswiesehan <lukas.wiesehan@icloud.com>
2020-09-28 18:31:43 +0200

    Finish landing page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,11 @@
   <section v-if="dark" id="navbar" class="bg-gradient-to-tr from-gray-600 to-gray-700 pb-20 xs:pb-32 md:pb-64">
     <div class="max-w-screen-xl mx-auto px-4 sm:px-8">
       <div class="py-6 md:py-12 lg:py-20 text-xs sm:text-base">
-        <a id="nav-item-1" v-if="showBackButton" href="#" class="inline-block uppercase font-display text-dark-300 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-dark-100 mr-8"><i class="fas fa-chevron-left"></i> Zurück</a>
-        <a id="nav-item-2" href="#" v-scroll-to="'#services'" class="inline-block uppercase font-display text-light-900 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-white mr-4 sm:mr-8">Services</a>
-        <a id="nav-item-3" href="#" v-scroll-to="'#references'" class="inline-block uppercase font-display text-light-900 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-white mr-4 sm:mr-8">Referenzen</a>
-        <a id="nav-item-4" href="#" v-scroll-to="'#about'" class="inline-block uppercase font-display text-light-900 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-white mr-4 sm:mr-8">Team</a>
-        <a id="nav-item-5" href="#" v-scroll-to="'#contact'" class="inline-block uppercase font-display text-light-900 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-white">Kontakt</a>
+        <a id="nav-item-1" v-if="showBackButton" href="#" class="inline-block uppercase font-display mr-8"><span class="inline-block text-dark-100 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-white"><i class="fas fa-chevron-left"></i> Zurück</span></a>
+        <a id="nav-item-2" href="#" v-scroll-to="'#services'" class="inline-block uppercase font-display mr-4 sm:mr-8"><span class="inline-block text-light-900 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-white">Services</span></a>
+        <a id="nav-item-3" href="#" v-scroll-to="'#references'" class="inline-block uppercase font-display mr-4 sm:mr-8"><span class="inline-block text-light-900 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-white">Referenzen</span></a>
+        <a id="nav-item-4" href="#" v-scroll-to="'#about'" class="inline-block uppercase font-display mr-4 sm:mr-8"><span class="inline-block text-light-900 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-white">Team</span></a>
+        <a id="nav-item-5" href="#" v-scroll-to="'#contact'" class="inline-block uppercase font-display"><span class="inline-block text-light-900 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:text-white">Kontakt</span></a>
       </div>
     </div>
   </section>

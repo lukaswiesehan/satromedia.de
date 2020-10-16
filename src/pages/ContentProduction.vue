5315fe8fd574e5eac3687a5768c6cada0353e276
lukaswiesehan <lukas.wiesehan@icloud.com>
2020-10-16 20:05:21 +0200

    Fix Index animations

--- src/pages/ContentProduction.vue
+++ src/pages/ContentProduction.vue
@@ -189,14 +189,14 @@
           }
         })
         //Scroll reveal animations:
-        gsap.from('#header-image', {scrollTrigger: '#header-image', y: 100, opacity: 0, duration: 1.6, ease: 'power3'})
-        gsap.from('#header-box', {scrollTrigger: '#header-box', y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3', delay: 0.2})
-        gsap.from('#stats', {scrollTrigger: '#stats', y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3', delay: 0.4})
-        gsap.from('#projects-title', {scrollTrigger: '#projects-title', y: 100, opacity: 0, duration: 1.6, ease: 'power3'})
-        gsap.from('#featured-project-1', {scrollTrigger: '#featured-project-1', y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3'})
-        gsap.from('#other-projects-1', {scrollTrigger: '#other-projects-1', y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3'})
-        gsap.from('#featured-project-2', {scrollTrigger: '#featured-project-2', y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3'})
-        gsap.from('#other-projects-2', {scrollTrigger: '#other-projects-2', y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3'})
+        gsap.from('#header-image', {scrollTrigger: {trigger: '#header-image'}, y: 100, opacity: 0, duration: 1.6, ease: 'power3'})
+        gsap.from('#header-box', {scrollTrigger: {trigger: '#header-box'}, y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3', delay: 0.2})
+        gsap.from('#stats', {scrollTrigger: {trigger: '#stats'}, y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3', delay: 0.4})
+        gsap.from('#projects-title', {scrollTrigger: {trigger: '#projects-title'}, y: 100, opacity: 0, duration: 1.6, ease: 'power3'})
+        gsap.from('#featured-project-1', {scrollTrigger: {trigger: '#featured-project-1'}, y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3'})
+        gsap.from('#other-projects-1', {scrollTrigger: {trigger: '#other-projects-1'}, y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3'})
+        gsap.from('#featured-project-2', {scrollTrigger: {trigger: '#featured-project-2'}, y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3'})
+        gsap.from('#other-projects-2', {scrollTrigger: {trigger: '#other-projects-2'}, y: 100, opacity: 0, scale: 0.95, duration: 1.6, ease: 'power3'})
         gsap.from('#footer', {scrollTrigger: '#footer', opacity: 0, duration: 1.6, ease: 'power3'})
       }
     },

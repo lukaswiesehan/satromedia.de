26f5f6a9272b9b960a1959186e40dc1d93dc1bba
Lukas Wiesehan <lukas.wiesehan@icloud.com>
2021-11-05 08:03:04 +0000

    Minor updates

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -51,7 +51,7 @@
             <div class="absolute top-0 left-0 w-full h-full bg-opacity-25 bg-dark-900"></div>
             <div class="absolute top-0 left-0 flex flex-col-reverse w-full h-full transition-all duration-500 ease-in-out bg-opacity-25 opacity-0 bg-dark-900 hover:opacity-100">
               <div class="p-4 text-xs text-white md:text-base">
-                <p class="text-xs font-bold text-white md:text-base">Henning Trogisch</p>
+                <p class="text-xs font-bold text-white md:text-base">Henning Aschhoff</p>
                 <p class="text-base text-light-500 md:text-lg">
                   <a href="javascript:mailto('nbjmup;ifoojohAtbuspnfejb/ef', '')" class="hover:text-light-900"><i class="fas fa-envelope"></i></a>
                   <a href="https://de.linkedin.com/in/henning-trogisch-b738a8172" target="_blank" class="ml-2 hover:text-light-900"><i class="fab fa-linkedin"></i></a>
